aec917c0b2a5833dee8e92220406b8bd754d0ada
Gilad Manor <gilad@quizic.co>
2013-08-11 23:45:44 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CyberGarden::Application.config.secret_key_base = 'b3b9fc7b97d39d67b197f685480a96ab29f6cde34c7c186bf69a9c0993b46a72b59d04fb4c0b013d72488ff24e80072ae91b2a4cae0471a942d65372aea95707'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CyberGarden::Application.config.session_store :cookie_store, key: '_cyber_garden_session'
