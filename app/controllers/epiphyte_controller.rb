7cfc977d93a378f12b67234dd0bb7ca489b52ae9
Gilad Manor <gilad@quizic.co>
2013-08-13 01:01:34 +0300

    a little action

--- app/controllers/epiphyte_controller.rb
+++ app/controllers/epiphyte_controller.rb
@@ -1,13 +1,32 @@
 class EpiphyteController < ApplicationController
-
+  before_action :set_user, only: [:start, :stop, :status]
+  respond_to :json
 
   def register
+    @user = User.create({:key=>User.generate_key})
+    respond_with(@user)
+  end
+  
+  def status
+    respond_with(@user.punch_cards.last)
   end
   
   def start
+    @user.punch_cards << PunchCard.new({:start_at=>Time.now})
+    respond_with(@user.punch_cards.last)
   end
   
   def stop
+    pc = @user.punch_cards.last
+    pc.end_at = Time.now
+    pc.save
+    respond_with(@user.punch_cards.last)
+  end
+  
+  private
+  
+  def set_user
+    @user = User.find(params[:id])
   end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+  has_many :punch_cards
+  
+  
+  def self.generate_key
+    o =  ([('A'..'Z')]+[(0..9)]).map{|i| i.to_a}.flatten  
+    (0..50).map{ o[rand(o.length)]}.join
+  end
 end
